604edae1a7c53364bcc8553cfff40d2f87a0fdf9
Archmagos-ai <blackcog25@proton.me>
2025-07-29 23:29:01 +1000

    Update WarVaultToken.sol

--- contracts/WarVaultToken.sol
+++ contracts/WarVaultToken.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract WarVaultToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("WarVault Token", "WAR") {
-        _mint(msg.sender, initialSupply);
-    }
+    constructor(uint256 initialSupply) ERC20("WarVault Token", "WAR") Ownable(msg.sender) {
+    _mint(msg.sender, initialSupply);
 }
